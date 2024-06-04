8400e218358e0c66b6e7e410937c74778aa9e773
Luiz Henrique Vidal Araújo <luizhva02@gmail.com>
2024-06-03 22:16:16 -0300

    22:16-03/06/2024

--- controller/controller_usuarios.js
+++ controller/controller_usuarios.js
@@ -12,26 +12,308 @@
 const usuariosDao = require('../model/DAO/usuarios.js')
 const message = require('../modulo/config.js')
 
+
+const antigoDigito = `'`
+const novoDigito = `|`
+
+const antigoCarater = `"`
+const novoCarater = ` || `
+
 const getListarUsuarios = async function () {
     let jsonUsuarios = {}
 
-    const dadosUsuarios = await usuariosDao.selectAllUsuarios()
+    try {
+        const dadosUsuarios = await usuariosDao.selectAllUsuarios()
 
-    if (dadosUsuarios) {
-        if (dadosUsuarios.length > 0) {
-            jsonUsuarios.usuarios = dadosUsuarios
-            jsonUsuarios.quantidade = dadosUsuarios.length
-            jsonUsuarios.status_code = 200
+        if (dadosUsuarios) {
+            if (dadosUsuarios.length > 0) {
+                jsonUsuarios.usuarios = dadosUsuarios
+                jsonUsuarios.quantidade = dadosUsuarios.length
+                jsonUsuarios.status_code = 200
 
-            return jsonUsuarios
+                return jsonUsuarios
+            } else {
+                return message.ERROR_NOT_FOUND
+            }
         } else {
-            return message.ERROR_NOT_FOUND
+            return message.ERROR_INTERNAL_SERVER_DB
+        }
+    } catch (error) {
+        console.log(error);
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getBuscarUsuarioById = async function (id) {
+    let jsonUsuario = {}
+
+    try {
+        if (id == "" || id == undefined || isNaN(id)) {
+            return message.ERROR_INVALID_ID
+        } else {
+            const dadosUsuario = await usuariosDao.selectByIdUsuario(id)
+
+            if (dadosUsuario) {
+                if (dadosUsuario.length > 0) {
+                    jsonUsuario.Usuario = dadosUsuario
+                    jsonUsuario.status_code = 200
+
+                    return jsonUsuario
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                console.log(dadosUsuario);
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getBuscarUltimoUsuarioInserido = async function () {
+    let jsonUsuario = {}
+
+    try {
+        const idUltimoUsuario = await usuariosDao.selectLastIdUsuario()
+
+        if (idUltimoUsuario) {
+            if (idUltimoUsuario.length > 0) {
+                jsonUsuario.idUsuario = idUltimoUsuario
+                jsonUsuario.status_code = 200
+
+                return jsonUsuario
+            } else {
+                return message.ERROR_NOT_FOUND
+            }
+        } else {
+            return message.ERROR_INTERNAL_SERVER_DB
+        }
+    } catch (error) {
+        console.log(error);
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const setInserirUsuarios = async function (contentType, dadosUsuario) {
+    try {
+        if (String(contentType).toLocaleLowerCase() === "application/json") {
+
+            let novoUsuarioJson = {}
+            if (dadosUsuario.nome == "" || dadosUsuario.nome == undefined ||
+                dadosUsuario.nome == null || dadosUsuario.nome.length > 100 ||
+                dadosUsuario.email == "" || dadosUsuario.email == undefined ||
+                dadosUsuario.email == null || dadosUsuario.email.length > 100 ||
+                dadosUsuario.telefone == "" || dadosUsuario.telefone == undefined ||
+                dadosUsuario.telefone == null || dadosUsuario.telefone.length > 20 ||
+                dadosUsuario.endereco == "" || dadosUsuario.endereco == undefined ||
+                dadosUsuario.endereco == null || dadosUsuario.endereco.length > 300 ||
+                dadosUsuario.cpf == "" || dadosUsuario.cpf == undefined ||
+                dadosUsuario.cpf == null || dadosUsuario.cpf.length > 15 ||
+                dadosUsuario.senha == "" || dadosUsuario.senha == undefined ||
+                dadosUsuario.senha == null || dadosUsuario.senha.length > 100
+            ) {
+                return message.ERROR_REQUIRED_FIELDS
+            } else {
+                let novoUsuario = await usuariosDao.insertUsuario(dadosUsuario)
+
+                if (novoUsuario) {
+                    let idNovoUsuario = await usuariosDao.selectLastIdUsuario()
+
+                    if (idNovoUsuario) {
+                        novoUsuarioJson.status = message.SUCCES_CREATED_ITEM.status
+                        novoUsuarioJson.status_code = message.SUCCES_CREATED_ITEM.status_code
+                        novoUsuarioJson.message = message.SUCCES_CREATED_ITEM.message
+                        novoUsuarioJson.id = idNovoUsuario[0].id
+                        novoUsuarioJson.novoUsuario = dadosUsuario
+
+                        console.log(novoUsuario);
+                        return novoUsuarioJson
+                    } else {
+                        console.log("Novo ID:" + idNovoUsuario);
+                        return message.ERROR_INTERNAL_SERVER_DB
+                    }
+                } else {
+                    console.log("Novo Usuario:" + dadosUsuario);
+                    return message.ERROR_INTERNAL_SERVER_DB
+                }
+            }
+        } else {
+            return message.ERROR_CONTENT_TYPE
+        }
+    } catch (error) {
+        console.log(error);
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const setAtualizarUsuario = async function (id, dadosUsuarioUpdate, content) {
+    if (String(content).toLowerCase() == 'application/json') {
+
+        let updateUsuarioJson = {}
+        let resultUpdateUsuarioJson = {}
+        try {
+            const validaId = await getBuscarUsuarioById(id)
+
+            if (validaId) {
+
+                let id = validaId.usuario[0].id
+                let nome = dadosUsuarioUpdate.nome
+                let email = dadosUsuarioUpdate.email
+                let telefone = dadosUsuarioUpdate.telefone
+                let endereco = dadosUsuarioUpdate.endereco
+                let cpf = dadosUsuarioUpdate.cpf
+                let senhaOriginal = dadosUsuarioUpdate.senha
+
+                if (
+                    nome != '' &&
+                    nome != undefined &&
+                    nome != null &&
+                    nome.length < 100
+                ) {
+                    updateUsuarioJson.nome = nome.replace(/'/g, novoDigito)
+                } else if (
+                    nome == '' &&
+                    nome == undefined &&
+                    nome == null
+                ) { }
+
+                if (
+                    email != '' &&
+                    email != undefined &&
+                    email != null &&
+                    email.length < 100
+                ) {
+
+                    updateUsuarioJson.email = email.replace(/'/g, novoDigito)
+                } else if (
+                    email == '' &&
+                    email == undefined &&
+                    email == null
+                ) { }
+
+                if (
+                    telefone != '' &&
+                    telefone != undefined &&
+                    telefone != null &&
+                    telefone.length == 20
+                ) {
+                    updateUsuarioJson.telefone = telefone
+                } else if (
+                    telefone == '' &&
+                    telefone == undefined &&
+                    telefone == null
+                ) { }
+
+                if (
+                    endereco != '' &&
+                    endereco != undefined &&
+                    endereco != null &&
+                    endereco.length == 300
+                ) {
+                    updateUsuarioJson.endereco = endereco
+                } else if (
+                    endereco == '' &&
+                    endereco == undefined &&
+                    endereco == null
+                ) { }
+
+                if (
+                    cpf != '' &&
+                    cpf != undefined &&
+                    cpf != null &&
+                    cpf.length == 15
+                ) {
+                    updateUsuarioJson.cpf = cpf
+                } else if (
+                    cpf == '' &&
+                    cpf == undefined &&
+                    cpf == null
+                ) { }
+
+                if (
+                    senhaOriginal != '' &&
+                    senhaOriginal != undefined &&
+                    senhaOriginal != null &&
+                    senhaOriginal.length == 100
+                ) {
+                    let senha = senhaOriginal.replace(/'/g, novoDigito)
+                    updateUsuarioJson.senha = senha.replace(/"/g, novoCarater)
+
+                } else if (
+                    senhaOriginal == '' &&
+                    senhaOriginal == undefined &&
+                    senhaOriginal == null
+                ) { }
+
+                const usuarioAtualizado = await usuariosDao.updateUsuario(id, updateUsuarioJson)
+
+                if (usuarioAtualizado) {
+                    resultUpdateUsuarioJson.id = id
+                    resultUpdateUsuarioJson.status = message.SUCCES_UPDATED_ITEM.status
+                    resultUpdateUsuarioJson.status_code = message.SUCCES_UPDATED_ITEM.status_code
+                    resultUpdateUsuarioJson.message = message.SUCCES_UPDATED_ITEM.message
+                    resultUpdateUsuarioJson.usuario = dadosUsuarioUpdate
+
+                    return resultUpdateUsuarioJson
+                } else {
+                    return message.ERROR_INTERNAL_SERVER_DB
+                }
+            } else {
+                return message.ERROR_NOT_FOUND
+            }
+
+        } catch (error) {
+            console.log(error);
+            return message.ERROR_UPDATED_ITEM
         }
     } else {
-        return message.ERROR_INTERNAL_SERVER_DB
+        return message.ERROR_CONTENT_TYPE
+    }
+}
+
+const setDeletarUsuarioById = async function (id) {
+    let jsonDeleteUsuario = {}
+
+    try {
+        if (id == "" || id == undefined || isNaN(id)) {
+            return message.ERROR_INVALID_ID
+        } else {
+            const validaId = await getBuscarUsuarioById(id)
+
+            if (validaId) {
+                const id = validaId.usuario[0].id
+
+                const apagarUsuario = await usuariosDao.deleteUsuario(id)
+
+                if (apagarUsuario) {
+                    jsonDeleteUsuario.status = message.SUCCES_DELETED_ITEM.status
+                    jsonDeleteUsuario.status_code = message.SUCCES_DELETED_ITEM.status_code
+                    jsonDeleteUsuario.message = message.SUCCES_DELETED_ITEM.message
+                    jsonDeleteUsuario.id = validaId.usuario[0].id
+
+                    return jsonDeleteUsuario
+                } else {
+                    console.log(dadosUsuario);
+                    return message.ERROR_INTERNAL_SERVER_DB
+                }
+            } else {
+
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return message.ERROR_INTERNAL_SERVER
     }
 }
 
 module.exports = {
-    getListarUsuarios
+    getListarUsuarios,
+    getBuscarUsuarioById,
+    getBuscarUltimoUsuarioInserido,
+    setInserirUsuarios,
+    setAtualizarUsuario,
+    setDeletarUsuarioById
 }
--- model/DAO/usuarios.js
+++ model/DAO/usuarios.js
@@ -15,41 +15,143 @@ const { PrismaClient } = require('@prisma/client')
 //Instânciando a classe do PrismaCliente
 const prisma = new PrismaClient()
 
-const selectAllUsuarios= async function () {
+const selectAllUsuarios = async function () {
     try {
-        
+        let sql = `select * from tbl_usuarios`
+
+        const rsUsuarios = await prisma.$queryRawUnsafe(sql)
+
+        return rsUsuarios
     } catch (error) {
         return false
     }
 }
 
-const selectByIdUsuario = async function () {
+const selectByIdUsuario = async function (id) {
     try {
-        
+        let sql = `select * from tbl_usuarios where id = ${id}`
+
+        const rsUsuarios = await prisma.$queryRawUnsafe(sql)
+
+        return rsUsuarios
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const selectLastIdUsuario = async function () {
+    try {
+        let sql = `select cast(last_insert_id() as decimal) as id from tbl_usuarios limit 1;`
+
+        const rsUsuarios = await prisma.$queryRawUnsafe(sql)
+
+        return rsUsuarios
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const updateUsuario = async function (id, dadosUsuarioUpdate) {
+    try {
+        let sql = `UPDATE tbl_usuarios SET `
+        const keys = Object.keys(dadosUsuarioUpdate)
+
+        keys.forEach((key, index) => {
+            if(key != 'senha'){
+                sql += `${key} = '${dadosUsuarioUpdate[key]}'`
+            }
+
+            if(key == 'senha'){
+                sql += `${key} = aes_encrypt('${dadosUsuarioUpdate[key]}','chave')`
+            }
+
+            if (index !== keys.length - 1) {
+                sql += `, `
+            }
+        })
+
+        sql += ` WHERE id = ${id}`
+
+        const rsUsuarios = await prisma.$executeRawUnsafe(sql)
+
+        return rsUsuarios
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const insertUsuario = async function (dadosUsuario) {
+    try {
+        let sql = `insert into tbl_usuarios  (
+                                                        nome,
+                                                        email,
+                                                        telefone,
+                                                        endereco,
+                                                        cpf,
+                                                        senha
+                                                    )
+                                                        values
+                                                    (
+                                                        '${dadosUsuario.nome}',
+                                                        '${dadosUsuario.email}',
+                                                        '${dadosUsuario.telefone}',
+                                                        '${dadosUsuario.endereco}',
+                                                        '${dadosUsuario.cpf}',
+                                                        AES_ENCRYPT('${dadosUsuario.senha}', 'chave')
+                                                    )`
+
+        const rsUsuarios = await prisma.$executeRawUnsafe(sql)
+
+        console.log(rsUsuarios);
+
+        return rsUsuarios
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const insertUsuarioLoop = async function (dadosUsuario) {
+    try {
+        let sql = `insert into tbl_usuarios  (
+                                                        nome,
+                                                        email,
+                                                        telefone,
+                                                        endereco,
+                                                        cpf
+                                                    )
+                                                        values
+                                                    (`
+                                                    const keys = Object.keys(dadosUsuario)
+                                            
+                                                    keys.forEach((key, index) => {
+                                                        sql += `${key} = '${dadosUsuario[key]}'`
+                                                        if (index !== keys.length - 1) {
+                                                            sql += `, `
+                                                        }
+                                                    })
+
+                                                    sql +=`)`
+
+        const rsUsuarios = await prisma.$queryRawUnsafe(sql)
+
+        console.log(rsUsuarios, sql)
+        return rsUsuarios
     } catch (error) {
         return false
     }
 }
 
-const updateUsuario = async function () {
+const deleteUsuario = async function (id) {
     try {
-        
-    } catch (error) {
-        return false
-    }
-}
+        let sql = `delete from tbl_usuarios where id = ${id}`
 
-const insertUsuario = async function () {
-    try {
-        
-    } catch (error) {
-        return false
-    }
-}
+        const rsUsuarios = await prisma.$executeRawUnsafe(sql)
 
-const deleteUsuario = async function () {
-    try {
-        
+        return rsUsuarios
     } catch (error) {
         return false
     }
@@ -58,7 +160,9 @@ const deleteUsuario = async function () {
 module.exports = {
     selectAllUsuarios,
     selectByIdUsuario,
+    selectLastIdUsuario,
     updateUsuario,
     insertUsuario,
+    insertUsuarioLoop,
     deleteUsuario
 }
